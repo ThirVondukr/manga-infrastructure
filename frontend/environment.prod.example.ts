15d52b066b774752d88d1240eed001e4068e0301
Doctor <thirvondukr@gmail.com>
2021-10-16 18:26:34 +0300

    Add frontend dockerfile, nginx.conf and example environment

---
+++ frontend/environment.prod.example.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    backendUrl: "http://localhost/api",
+    graphqlUrl: "http://localhost/graphql",
+    cdnUrl: "http://localhost/static",
+};
+
